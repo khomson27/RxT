606ddfd31a0dc46e9cb601d60599ea31c7d06727
khomsan <khomsan_su@cmu.ac.th>
2021-01-22 16:57:13 +0700

    complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -4,7 +4,20 @@ using namespace std;
 template <typename T>
 void insertionSort(T d[],int N){
 
+    for (int j=1;j<N;j++) {
+		for (int  i=j;i>0;i--) {
+			if (d[i] > d[i-1]) {
+			    double sw = d[i];
+			    d[i] = d[i-1];
+			    d[i-1] = sw;
+			}
+		}
+		cout << "Pass " << j << ":"; 
+		for (int i=0;i<N;i++) cout << d[i] << " ";
+		cout << endl;
+	}
 }
+	
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
